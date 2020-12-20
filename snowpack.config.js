e2942e0c4f3c97a6079467c9a2b8ca1107e534b9
Cheri Hung <cherihung@gmail.com>
2020-12-20 12:12:33 -0500

    snowpack configs

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,29 +2,40 @@
 module.exports = {
   mount: {
     public: {url: '/', static: true},
-    src: {url: '/dist'},
+    web: {url: '/_dist_'},
   },
   plugins: [
-    '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: 'tsc -p tsconfig.web.json --noEmit',
+        watch: '$1 --watch'
+      }
+    ],
+    [
+      '@snowpack/plugin-react-refresh',
+      '@snowpack/plugin-dotenv',
+      '@snowpack/plugin-webpack'
+    ]
   ],
   install: [
     /* ... */
   ],
   installOptions: {
-    /* ... */
+    installTypes: true
   },
   devOptions: {
-    /* ... */
+    port: 8080,
+    out: 'GENERATED',
+    open: 'none'
   },
   buildOptions: {
     /* ... */
   },
   proxy: {
-    /* ... */
+    '/api': 'http://localhost:3000/api'
   },
   alias: {
-    /* ... */
+    '@web': './web'
   },
 };
