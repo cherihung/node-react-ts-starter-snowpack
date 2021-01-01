70505591fb61ccef51b7b841e32902e864dc76cc
Cheri Hung <cherihung@gmail.com>
2020-12-31 21:01:24 -0500

    remove builds

--- server/SERVER_BUILD/configs/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.LOG_FORMAT = void 0;
-exports.LOG_FORMAT = process.env.LOG_FORMAT || 'combined';
-exports.PORT = process.env.PORT || '3000';
